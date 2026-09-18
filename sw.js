a8b2ed5b812ef7cda3c24a7f2881414e6df2ca45
Shopatat <coincoinwcbiensur@free.fr>
2026-09-18 20:33:28 +0200

    Ajoute la musique d'attente du 1er mot des qualifs (mise en cache PWA incluse)

--- sw.js
+++ sw.js
@@ -8,6 +8,7 @@ const ASSETS = [
   './sounds/music-finale-late.mp3',
   './sounds/music-menu.mp3',
   './sounds/music-qualif-alt.mp3',
+  './sounds/music-qualif-wait.mp3',
   './sounds/music-qualif.mp3',
   './sounds/sfx-correct.wav',
   './sounds/sfx-finale-timeout.wav',
